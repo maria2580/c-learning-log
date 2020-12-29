8f017c06b422d63ed496ec2bb1c2e9a7fb8fe9d1
maria2580 <67868660+maria2580@users.noreply.github.com>
2020-12-29 11:25:40 +0900

    test upload

---
+++ Project5/Project5/소스.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+
+#define endl "\n"
+#define MAX 110
+using namespace std;
+
+int N;
+int Parent[MAX];
+double Answer;
+vector<pair<double, double>> Coord;
+vector<pair<double, pair<int, int>>> Edge;
+
+void Input()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        double a, b; cin >> a >> b;
+        Coord.push_back(make_pair(a, b));
+    }
+}
+
+double Find_Distance(double x, double y, double xx, double yy)
+{
+    double dx = (x - xx) * (x - xx);
+    double dy = (y - yy) * (y - yy);
+    double Dist = sqrt(dx + dy);
+
+    return Dist;
+}
+
+int Find_Parent(int A)
+{
+    if (A == Parent[A]) return A;
+    else return Parent[A] = Find_Parent(Parent[A]);
+}
+
+bool Same_Parent(int A, int B)
+{
+    A = Find_Parent(A);
+    B = Find_Parent(B);
+    if (A == B) return true;
+    return false;
+}
+
+void Union(int A, int B)
+{
+    A = Find_Parent(A);
+    B = Find_Parent(B);
+
+    Parent[B] = A;
+}
+
+void Solution()
+{
+    for (int i = 0; i < Coord.size(); i++)
+    {
+        double x = Coord[i].first;
+        double y = Coord[i].second;
+
+        for (int j = i + 1; j < Coord.size(); j++)
+        {
+            double xx = Coord[j].first;
+            double yy = Coord[j].second;
+
+            double Dist = Find_Distance(x, y, xx, yy);
+            Edge.push_back(make_pair(Dist, make_pair(i, j)));
+        }
+    }
+
+    for (int i = 0; i < N; i++) Parent[i] = i;
+    sort(Edge.begin(), Edge.end());
+    for (int i = 0; i < Edge.size(); i++)
+    {
+        double Cost = Edge[i].first;
+        int Node1 = Edge[i].second.first;
+        int Node2 = Edge[i].second.second;
+
+        if (Same_Parent(Node1, Node2) == false)
+        {
+            Union(Node1, Node2);
+            Answer = Answer + Cost;
+        }
+    }
+    cout << Answer << endl;
+}
+
+void Solve()
+{
+    Input();
+    Solution();
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout << fixed;
+    cout.precision(2);
+
+    //    freopen("Input.txt", "r", stdin);
+    Solve();
+
+    return 0;
+}
